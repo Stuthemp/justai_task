bf4a45eb1b42550978348d988e8643e6e6f8d9bd
Stuthemp <semenshulga1@gmail.com>
2022-10-21 19:04:28 +0300

    first try

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+
 plugins {
     application
     kotlin("jvm") version "1.4.21"
     id("com.justai.jaicf.jaicp-build-plugin") version "0.1.1"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+    id("org.springframework.boot") version "2.6.13"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+
 }
 
 group = "com.justai.jaicf"
@@ -24,6 +30,11 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("com.h2database:h2")
 
     implementation("ch.qos.logback:logback-classic:$logback")
 
--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -1,58 +1,68 @@
 package com.justai.jaicf.template.scenario
 
+import com.justai.jaicf.builder.createModel
 import com.justai.jaicf.activator.caila.caila
-import com.justai.jaicf.builder.Scenario
+import com.justai.jaicf.model.scenario.Scenario
+import com.justai.jaicf.template.configuration.BotConfiguration
+import org.springframework.stereotype.Component
 
-val mainScenario = Scenario {
-    state("start") {
-        activators {
-            regex("/start")
-            intent("Hello")
+/**
+ * todo Document type MainScenario
+ */
+@Component
+class MainScenario(
+    private val botConfiguration: BotConfiguration
+): Scenario {
+
+    override val model = createModel {
+        state("start") {
+            activators {
+                regex("/start")
+                intent("Hello")
+            }
+            action {
+                reactions.run {
+                    sayRandom(
+                        "Hello! How can I help?",
+                        "Hi there! How can I help you?"
+                    )
+                    buttons(
+                        "Help me!",
+                        "How are you?",
+                        "What is your name?"
+                    )
+                }
+            }
         }
-        action {
-            reactions.run {
-                image("https://media.giphy.com/media/ICOgUNjpvO0PC/source.gif")
-                sayRandom(
-                    "Hello! How can I help?",
-                    "Hi there! How can I help you?"
-                )
-                buttons(
-                    "Help me!",
-                    "How are you?",
-                    "What is your name?"
+
+        state("bye") {
+            activators {
+                intent("Bye")
+            }
+
+            action {
+                reactions.sayRandom(
+                    "See you soon!",
+                    "Bye-bye!"
                 )
             }
         }
-    }
 
-    state("bye") {
-        activators {
-            intent("Bye")
+        state("smalltalk", noContext = true) {
+            activators {
+                anyIntent()
+            }
+
+            action(caila) {
+                activator.topIntent.answer?.let { reactions.say(it) } ?: reactions.go("/fallback")
+            }
         }
 
-        action {
+        fallback {
             reactions.sayRandom(
-                "See you soon!",
-                "Bye-bye!"
+                "Sorry, I didn't get that...",
+                "Sorry, could you repeat please?"
             )
-            reactions.image("https://media.giphy.com/media/EE185t7OeMbTy/source.gif")
         }
-    }
-
-    state("smalltalk", noContext = true) {
-        activators {
-            anyIntent()
-        }
-
-        action(caila) {
-            activator.topIntent.answer?.let { reactions.say(it) } ?: reactions.go("/fallback")
-        }
-    }
-
-    fallback {
-        reactions.sayRandom(
-            "Sorry, I didn't get that...",
-            "Sorry, could you repeat please?"
-        )
     }
 }
